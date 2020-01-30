46548e82127be11c01286c1de11ad8db23703e47
cgutierrez7 <chris.n.gutierrez@gmail.com>
2020-01-30 08:39:58 -0800

    move ShowWindow method from Program to GameEngine

--- TheSyndicate/Program.cs
+++ TheSyndicate/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            ConsoleWindow.ShowWindow(ConsoleWindow.ThisConsole, ConsoleWindow.MAXIMIZE);
             GameEngine gameEngine = new GameEngine();
             gameEngine.Start();
         }
