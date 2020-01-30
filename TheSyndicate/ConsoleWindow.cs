91136de783d72e61a25c7288fa3c0ad42e6ad6a6
cgutierrez7 <chris.n.gutierrez@gmail.com>
2020-01-29 23:32:22 -0800

    Added ConsoleWindow class with ShowWindow method to maximize console window

---
+++ TheSyndicate/ConsoleWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TheSyndicate
+{
+    class ConsoleWindow
+    {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+        public static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        public const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+    }
+}
--- TheSyndicate/Program.cs
+++ TheSyndicate/Program.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
-
-
-namespace TheSyndicate
+﻿namespace TheSyndicate
 {
     class Program
     {
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-
-        private static extern IntPtr GetConsoleWindow();
-        private static IntPtr ThisConsole = GetConsoleWindow();
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        private const int HIDE = 0;
-        private const int MAXIMIZE = 3;
-        private const int MINIMIZE = 6;
-        private const int RESTORE = 9;
-
         static void Main(string[] args)
         {
-            ShowWindow(ThisConsole, MAXIMIZE);
+            ConsoleWindow.ShowWindow(ConsoleWindow.ThisConsole, ConsoleWindow.MAXIMIZE);
             GameEngine gameEngine = new GameEngine();
             gameEngine.Start();
         }
