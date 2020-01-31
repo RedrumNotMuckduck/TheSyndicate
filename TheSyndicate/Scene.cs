a5ece7af235049484160e9b7dbf0e805d06f20f8
cgutierrez7 <chris.n.gutierrez@gmail.com>
2020-01-31 04:12:21 -0800

    Added partially complete text formatting functionality

--- TheSyndicate/Scene.cs
+++ TheSyndicate/Scene.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TheSyndicate
 {
     public class Scene
     {
+        Regex NEW_LINE_PATTERN = new Regex(@"\n");
         public string Id { get; private set; }
         public string Text { get; private set; }
         public string[] Options { get; private set; }
@@ -31,9 +34,59 @@ namespace TheSyndicate
         void RenderText()
         {
             ClearConsole();
-            Console.WriteLine(this.Text);
+            FormatTextForTextBox();
+            //Console.WriteLine(this.Text);
         }
         
+        void FormatTextForTextBox()
+        {
+            StringBuilder boxText = new StringBuilder();
+            double maxLineWidth = Console.WindowWidth - 10;
+            double lineCount = Math.Ceiling(this.Text.Length/maxLineWidth);
+            int lineWidth = Convert.ToInt32(maxLineWidth);
+            int textLength = this.Text.Length;
+            int newLineIndex;
+            int startIndex = 0;
+            int endIndex = lineWidth;
+
+            //for (int i = 1; i <= lineCount; i++)
+            while (startIndex < textLength)
+            {
+                //int startIndex = (i * lineWidth) - lineWidth;
+                //int endIndex = i * lineWidth > textLength ? textLength - 1 : i * lineWidth;
+                string dashChar;
+
+                //if (i < lineCount)
+                //{
+                    newLineIndex = NEW_LINE_PATTERN.Match(this.Text, startIndex, textLength - startIndex).Index;
+                    if (newLineIndex < endIndex && newLineIndex !=0)
+                    {
+                        boxText.Append(this.Text, startIndex, newLineIndex - startIndex + 1);
+                        startIndex = newLineIndex + 2;
+                        endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
+                    }
+                    else
+                    {
+                    //dashChar = !this.Text[endIndex].Equals(" ") && endIndex != textLength ? "-" : "";
+                    //dashChar = endIndex >= textLength ? "" : (!this.Text[endIndex + 1].Equals(" ") ? "-" : "");
+                        boxText.Append(this.Text, startIndex, endIndex - startIndex).AppendLine();
+                        //.Append(dashChar).AppendLine();
+                        startIndex = endIndex;
+                        endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
+                    }
+                //}
+                //else
+                //{
+                //    newLineIndex = NEW_LINE_PATTERN.Match(this.Text, startIndex, endIndex).Index;
+                //    boxText.Append(this.Text, startIndex, endIndex);
+                //}
+            }
+            //Console.SetCursorPosition(10, 4);
+            Console.WriteLine(boxText);
+        }
+
+        //Line break point using this.Text.lastindexof(" ")
+
         void RenderOptions()
         {
             if (this.Options.Length > 0) 
---
+++ TheSyndicate/TextBox.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TheSyndicate
+{
+    public class TextBox
+    {
+        char TOP_LEFT_CORNER = '\u2554';
+        char TOP_RIGHT_CORNER = '\u2557';
+        char BOTTOM_LEFT_CORNER = '\u255A';
+        char BOTTOM_RIGHT_CORNER = '\u255D';
+        char HORIZONTAL_LINE = '\u2550';
+        char VERTICAL_LINE = '\u2551';
+        private int Width { get; set; }
+        private int Height { get; set; }
+
+        public TextBox(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        void SetBoxPosition(int xCoord, int yCoord)
+        {
+            Console.SetCursorPosition(xCoord,yCoord);
+        }
+
+        public void DrawDialogBox()
+        {
+            StringBuilder box = new StringBuilder();
+            DrawBoxTop(box);
+            DrawBoxSides(box);
+            DrawBoxBottom(box);
+            Console.WriteLine(box);
+        }
+
+        public void DrawOptionsBox()
+        {
+            int height = (Console.WindowHeight/4) * 3;
+            Console.SetCursorPosition(0, height);
+            DrawDialogBox();
+        }
+        
+        void DrawBoxTop(StringBuilder box)
+        {
+            box.Append(TOP_LEFT_CORNER);
+            box.Append(HORIZONTAL_LINE, this.Width - 2);
+            box.Append(TOP_RIGHT_CORNER).AppendLine();
+        }
+
+        void DrawBoxSides(StringBuilder box)
+        {
+            int count = 0;
+
+            while (count++ < this.Height - 2)
+            {
+                box.Append(VERTICAL_LINE);
+                box.Append(' ', this.Width - 2);
+                box.Append(VERTICAL_LINE).AppendLine();
+            }
+        }
+
+        void DrawBoxBottom(StringBuilder box)
+        {
+            box.Append(BOTTOM_LEFT_CORNER);
+            box.Append(HORIZONTAL_LINE, this.Width - 2);
+            box.Append(BOTTOM_RIGHT_CORNER);
+        }
+    }
+}
