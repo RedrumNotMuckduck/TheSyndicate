938b252b4b2b5092b3390560945f6b02f424114f
cgutierrez7 <chris.n.gutierrez@gmail.com>
2020-02-03 15:15:29 -0800

    textBox class update with basic functionality

--- TheSyndicate/Scene.cs
+++ TheSyndicate/Scene.cs
@@ -23,99 +23,55 @@ namespace TheSyndicate
             this.ActualDestinationId = null;
             this.Start = start;
         }
-
+        
         public void Play()
         {
-            RenderText();
-            RenderOptions();
+            TextBox sceneTextBox = RenderText();
+            RenderOptions(sceneTextBox);
             GetUserInput();
         }
 
-        void RenderText()
+        TextBox RenderText()
         {
             ClearConsole();
-            FormatTextForTextBox();
-            //Console.WriteLine(this.Text);
+            TextBox dialogBox = new TextBox(this.Text, Console.WindowWidth - 4, (this.Text.Length / (Console.WindowWidth - 8)) + 4);
+            dialogBox.DrawDialogBox(this.Text);
+            dialogBox.FormatText(this.Text);
+            return dialogBox; //returning dialogBox for information about height of dialog box
         }
         
-        void FormatTextForTextBox()
-        {
-            StringBuilder boxText = new StringBuilder();
-            double maxLineWidth = Console.WindowWidth - 10;
-            double lineCount = Math.Ceiling(this.Text.Length/maxLineWidth);
-            int lineWidth = Convert.ToInt32(maxLineWidth);
-            int textLength = this.Text.Length;
-            int newLineIndex;
-            int startIndex = 0;
-            int endIndex = lineWidth;
-
-            //for (int i = 1; i <= lineCount; i++)
-            while (startIndex < textLength)
-            {
-                //int startIndex = (i * lineWidth) - lineWidth;
-                //int endIndex = i * lineWidth > textLength ? textLength - 1 : i * lineWidth;
-                string dashChar;
-
-                //if (i < lineCount)
-                //{
-                    newLineIndex = NEW_LINE_PATTERN.Match(this.Text, startIndex, textLength - startIndex).Index;
-                    if (newLineIndex < endIndex && newLineIndex !=0)
-                    {
-                        boxText.Append(this.Text, startIndex, newLineIndex - startIndex + 1);
-                        startIndex = newLineIndex + 2;
-                        endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
-                    }
-                    else
-                    {
-                    //dashChar = !this.Text[endIndex].Equals(" ") && endIndex != textLength ? "-" : "";
-                    //dashChar = endIndex >= textLength ? "" : (!this.Text[endIndex + 1].Equals(" ") ? "-" : "");
-                        boxText.Append(this.Text, startIndex, endIndex - startIndex).AppendLine();
-                        //.Append(dashChar).AppendLine();
-                        startIndex = endIndex;
-                        endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
-                    }
-                //}
-                //else
-                //{
-                //    newLineIndex = NEW_LINE_PATTERN.Match(this.Text, startIndex, endIndex).Index;
-                //    boxText.Append(this.Text, startIndex, endIndex);
-                //}
-            }
-            //Console.SetCursorPosition(10, 4);
-            Console.WriteLine(boxText);
-        }
-
-        //Line break point using this.Text.lastindexof(" ")
-
-        void RenderOptions()
+        void RenderOptions(TextBox sceneTextBox)
         {
             if (this.Options.Length > 0) 
             {
-                RenderUserOptions();
+                RenderUserOptions(sceneTextBox);
             }
             else
             {
-                RenderQuitMessage();
+                RenderQuitMessage(sceneTextBox);
             }
         }
 
-        private void RenderUserOptions()
+        private void RenderUserOptions(TextBox sceneTextBox)
         {
-            RenderInstructions();
+            RenderInstructions(sceneTextBox);
             for (int i = 0; i < this.Options.Length; i++)
             {
+                sceneTextBox.SetBoxPosition( 4 , ((Console.WindowHeight * 3 / 4) + 2 + i));
                 Console.WriteLine($"{i + 1}: {this.Options[i]}\n");
             }
         }
 
-        private void RenderInstructions()
+        private void RenderInstructions(TextBox sceneTextBox)
         {
-            Console.WriteLine("\n\nWhat will you do next? Enter the number next to the option and press enter:\n");
+            sceneTextBox.SetBoxPosition(4, Console.WindowHeight * 3 / 4);
+            Console.WriteLine("What will you do next? Enter the number next to the option and press enter:");
         }
 
-        private void RenderQuitMessage()
+        private void RenderQuitMessage(TextBox sceneTextBox)
         {
-            Console.WriteLine("\n\nYou have reached the end of your journey. Press CTRL + C to end.");
+            sceneTextBox.SetBoxPosition(4, Console.WindowHeight * 3 / 4);
+            Console.WriteLine("You have reached the end of your journey. Press CTRL + C to end.");
         }
 
         void GetUserInput()
--- TheSyndicate/TextBox.cs
+++ TheSyndicate/TextBox.cs
@@ -13,60 +13,136 @@ namespace TheSyndicate
         char BOTTOM_RIGHT_CORNER = '\u255D';
         char HORIZONTAL_LINE = '\u2550';
         char VERTICAL_LINE = '\u2551';
+
+        int TEXT_BOX_X_DEFAULT = 2;
+        int TEXT_BOX_Y_DEFAULT = 2;
+
+        Regex NEW_LINE_PATTERN = new Regex($"\n");
+
         private int Width { get; set; }
         private int Height { get; set; }
+        private int TextBufferX { get; set; }
+        private int TextBufferY { get; set; }
+        private int TextBoxX { get; set; }
+        private int TextBoxY { get; set; }
+        public int NewLines { get; set; }
 
-        public TextBox(int width, int height)
+        public TextBox(string text = "", int width = 100, int height = 20)
         {
             this.Width = width;
             this.Height = height;
+            this.TextBufferX = 2;
+            this.TextBufferY = 2;
+            this.TextBoxX = TEXT_BOX_X_DEFAULT;
+            this.TextBoxY = TEXT_BOX_Y_DEFAULT;
         }
 
-        void SetBoxPosition(int xCoord, int yCoord)
+        public void SetBoxPosition(int xCoord = 0, int yCoord = 0)
         {
-            Console.SetCursorPosition(xCoord,yCoord);
+            Console.SetCursorPosition(xCoord, yCoord);
         }
 
-        public void DrawDialogBox()
+        public void DrawDialogBox(string text)
         {
             StringBuilder box = new StringBuilder();
+            MatchCollection matches = NEW_LINE_PATTERN.Matches(text);
+            this.Height += matches.Count;
+
             DrawBoxTop(box);
             DrawBoxSides(box);
             DrawBoxBottom(box);
-            Console.WriteLine(box);
         }
 
-        public void DrawOptionsBox()
+        public void DrawBoxTop(StringBuilder box)
         {
-            int height = (Console.WindowHeight/4) * 3;
-            Console.SetCursorPosition(0, height);
-            DrawDialogBox();
-        }
-        
-        void DrawBoxTop(StringBuilder box)
-        {
+            box.Clear();
             box.Append(TOP_LEFT_CORNER);
+            //this.Width - 2 for corner characters
             box.Append(HORIZONTAL_LINE, this.Width - 2);
-            box.Append(TOP_RIGHT_CORNER).AppendLine();
+            box.Append(TOP_RIGHT_CORNER);
+            SetBoxPosition(TextBoxX, TextBoxY);
+            Console.Write(box);
+            TextBoxY++;
         }
 
-        void DrawBoxSides(StringBuilder box)
+        public void DrawBoxSides(StringBuilder box)
         {
             int count = 0;
 
-            while (count++ < this.Height - 2)
+            while (count++ < this.Height)
             {
+                box.Clear();
                 box.Append(VERTICAL_LINE);
+                //this.Width - 2 for right and left box borders
                 box.Append(' ', this.Width - 2);
-                box.Append(VERTICAL_LINE).AppendLine();
+                box.Append(VERTICAL_LINE);
+                SetBoxPosition(TextBoxX, TextBoxY);
+                Console.WriteLine(box);
+                TextBoxY++;
             }
         }
 
-        void DrawBoxBottom(StringBuilder box)
+        public void DrawBoxBottom(StringBuilder box)
         {
+            box.Clear();
             box.Append(BOTTOM_LEFT_CORNER);
+            //this.Width - 2 for corner characters
             box.Append(HORIZONTAL_LINE, this.Width - 2);
             box.Append(BOTTOM_RIGHT_CORNER);
+            SetBoxPosition(TextBoxX, TextBoxY);
+            Console.WriteLine(box);
+            TextBoxY = TEXT_BOX_Y_DEFAULT;
+        }
+
+        public void FormatText(string text)
+        {
+            StringBuilder boxText = new StringBuilder();
+            int lineWidth = this.Width - (TextBoxX + TextBufferX);
+            int textLength = text.Length;
+            int newLineIndex;
+            int startIndex = 0;
+            int endIndex = lineWidth;
+            int textStartX = TextBufferX + TextBoxX;
+            int textStartY = TextBufferY + TextBoxY;
+
+            while (startIndex < textLength)
+            {
+                string dashChar;
+
+                newLineIndex = NEW_LINE_PATTERN.Match(text, startIndex, endIndex - startIndex).Index;
+                //if (newLineIndex < endIndex && newLineIndex == 0 && (startIndex + newLineIndex + 2) < textLength)
+                //{
+                //    boxText.Append(' ', TextBoxX + TextBufferX);
+                //    boxText.Append(text, startIndex, newLineIndex + 2);
+                //    SetBoxPosition(textStartX, textStartY);
+                //    Console.Write(boxText);
+                //    startIndex += 2;
+                //    endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
+                //    textStartY++;
+                //    boxText.Clear();
+                //}
+                //else
+                if (newLineIndex < endIndex && newLineIndex != 0)
+                {
+                    boxText.Append(text, startIndex, newLineIndex - startIndex);
+                    SetBoxPosition(textStartX, textStartY);
+                    Console.Write(boxText);
+                    startIndex = newLineIndex + 2;
+                    endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
+                    textStartY++;
+                    boxText.Clear();
+                }
+                else
+                {
+                    boxText.Append(text, startIndex, endIndex - startIndex);
+                    SetBoxPosition(textStartX, textStartY);
+                    Console.Write(boxText);
+                    startIndex = endIndex;
+                    endIndex = startIndex + lineWidth > textLength ? textLength : startIndex + lineWidth;
+                    textStartY++;
+                    boxText.Clear();
+                }
+            }
         }
     }
 }
