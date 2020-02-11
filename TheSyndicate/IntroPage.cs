701cd755c491fca30375ff4c3561d2c284cbca47
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2020-02-11 15:00:31 -0800

    Moving robot working

---
+++ TheSyndicate/IntroPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSyndicate
+{
+    public class IntroPage
+    {
+        public IntroPage()
+        {
+            Board Intro_Board = new Board();
+            Intro_Board.Create_Intro_Box();
+        }
+    }
+}
