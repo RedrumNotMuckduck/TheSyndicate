a0a6930efb59ecc7f239a3e29224b9e1b14d96e8
xobre <mua.breana@gmail.com>
2020-02-19 10:55:22 -0800

    Sound Effects Added

---
+++ TheSyndicate/SoundEffects/Sounds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using NAudio.Wave;
+
+namespace TheSyndicate.SoundEffects
+{
+    class Sounds
+    {
+        internal static AudioFileReader audioFile;
+        internal static WaveOutEvent outputDevice;
+
+        internal static void PlaySound(string musicFile, int milliSeconds = 0)
+        {
+            // Takes in an audio file & controls the wait time to start
+            audioFile = new AudioFileReader(musicFile);
+            outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+            Thread.Sleep(milliSeconds);
+        }
+
+        internal static void DisposeAudio()
+        {
+            // Ends audio playback
+            audioFile.Dispose();
+            outputDevice.Dispose();
+        }
+    }
+}
